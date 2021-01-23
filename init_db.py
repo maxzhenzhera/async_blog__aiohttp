0e5d234d64e5aaa4036a1435a4854f5f0b078ebc
Max Zhenzhera <megashurik@urk.net>
2021-01-23 11:52:25 +0200

    Init database implementations is done

--- init_db.py
+++ init_db.py
@@ -3,71 +3,120 @@ Initialize (create db, tables, new role) and implement deleting all this stuff.
 As we don`t use ORM like SQLAlchemy, most of the needed SQL code is saved in a particular directory.
 """
 
-# from core.db import question, choice
+import asyncio
+
+import aiomysql
+from loguru import logger
+
+from core.db import Database, tables
 from core.settings import BASE_DIR, get_config
 
 
 USER_CONFIG_PATH = BASE_DIR / 'config' / 'config.ini'
 USER_CONFIG = get_config()
+DATABASE_CONFIG = USER_CONFIG['mysql']
 
-# TEST_CONFIG_PATH = BASE_DIR / 'config' / 'test_config.ini'
-# TEST_CONFIG = get_config()
+DB_NAME: str = DATABASE_CONFIG['database']
+DB_HOST: str = DATABASE_CONFIG['host']
+DB_PORT: int = int(DATABASE_CONFIG['port'])
+DB_USER_NAME: str = DATABASE_CONFIG['user_name']
+DB_USER_PASSWORD: str = DATABASE_CONFIG['user_password']
 
-
-def setup_db(config):
-
-    db_name = config['database']
-    db_user = config['user']
-    db_pass = config['password']
-    db_host = config['host']
-
-    conn = admin_engine.connect()
-
-    conn.execute("DROP DATABASE IF EXISTS %s" % db_name)
-    conn.execute("DROP ROLE IF EXISTS %s" % db_user)
-    conn.execute("CREATE USER '%s'@'%s' IDENTIFIED BY '%s'" % (db_user, db_host, db_pass))
-
-    conn.execute("CREATE DATABASE %s ENCODING 'UTF8'" % db_name)
-    conn.execute("GRANT ALL PRIVILEGES ON DATABASE %s TO %s" %
-                 (db_name, db_user))
+# data for first set up (used `root` user for creating database and user)
+DB_ROOT_NAME: str = DATABASE_CONFIG['root_name']
+DB_ROOT_PASSWORD: str = DATABASE_CONFIG['root_password']
 
 
-def teardown_db(config):
+async def setup_db(connection: aiomysql.Connection) -> None:
+    """ Creates database, tables and user """
+    query_create_database = Database.create_database.format(
+        db_name=DB_NAME
+    )
 
-    db_name = config['database']
-    db_user = config['user']
+    async with connection.cursor() as cursor:
+        await cursor.execute(query_create_database)
 
-    conn = admin_engine.connect()
-    conn.execute("""
-      SELECT pg_terminate_backend(pg_stat_activity.pid)
-      FROM pg_stat_activity
-      WHERE pg_stat_activity.datname = '%s'
-        AND pid <> pg_backend_pid();""" % db_name)
-    conn.execute("DROP DATABASE IF EXISTS %s" % db_name)
-    conn.execute("DROP ROLE IF EXISTS %s" % db_user)
-    conn.close()
+    await create_tables(connection)
+    await create_user(connection)
 
 
-def create_tables(engine=test_engine):
-    meta = MetaData()
-    meta.create_all(bind=engine, tables=[question, choice])
+async def teardown_db(connection: aiomysql.Connection) -> None:
+    """ Drops the entire base """
+    query_drop_database = Database.drop_database.format(
+        db_name=DB_NAME
+    )
+
+    async with connection.cursor() as cursor:
+        await cursor.execute(query_drop_database)
+
+    await drop_user(connection)
 
 
-def drop_tables(engine=test_engine):
-    meta = MetaData()
-    meta.drop_all(bind=engine, tables=[question, choice])
+async def create_tables(connection: aiomysql.Connection) -> None:
+    """ Creates all tables in the database """
+    query_use_database = "USE {db_name}".format(db_name=DB_NAME)
+
+    async with connection.cursor() as cursor:
+        await cursor.execute(query_use_database)
+        for table in tables:
+            await cursor.execute(table.create_table)
+
+
+async def drop_tables(connection: aiomysql.Connection) -> None:
+    """ Drops all tables in the database """
+    query_use_database = "USE {db_name}".format(db_name=DB_NAME)
+
+    async with connection.cursor() as cursor:
+        await cursor.execute(query_use_database)
+        for table in tables:
+            await cursor.execute(table.drop_table)
+
+
+async def create_user(connection: aiomysql.Connection) -> None:
+    """ Creates user that lead the app database """
+    query_create_user = Database.create_user.format(
+        db_name=DB_NAME,
+        user_name=DB_USER_NAME,
+        user_password=DB_USER_PASSWORD
+    )
+
+    async with connection.cursor() as cursor:
+        await cursor.execute(query_create_user)
+
+
+async def drop_user(connection: aiomysql.Connection) -> None:
+    """ Drops user that lead the app database """
+    query_drop_user = Database.drop_user.format(
+        user_name=DB_USER_NAME,
+    )
+
+    async with connection.cursor() as cursor:
+        await cursor.execute(query_drop_user)
+
+
+async def main(loop: asyncio.AbstractEventLoop) -> None:
+    """ All functions described above can be used here """
+    connection = await aiomysql.connect(
+        host=DB_HOST,
+        port=DB_PORT,
+        user=DB_ROOT_NAME,
+        password=DB_ROOT_PASSWORD,
+        loop=loop,
+        autocommit=True
+    )
+
+    await teardown_db(connection)
+
+    # await setup_db(connection)
+
+    # --------------------------------------------------
+    # # await drop_tables(cursor)
+    # # await teardown_db(cursor)
+    # --------------------------------------------------
+
+    connection.close()
 
 
 if __name__ == '__main__':
-    pass
-    # setup_db(USER_CONFIG['postgres'])
-    # create_tables(engine=user_engine)
-
-    # --------------------------------------------------
-    # --------------------------------------------------
-    # --------------------------------------------------
-
-    # drop_tables()
-    # teardown_db(USER_CONFIG['postgres'])
-
-    # --------------------------------------------------
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main(loop))
