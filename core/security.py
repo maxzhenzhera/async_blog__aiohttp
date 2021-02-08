29061c45f32718854dbf2c83dfba94a944222264
Max Zhenzhera <megashurik@urk.net>
2021-02-08 07:35:54 +0200

    File with different hash and encrypt functions

--- core/security.py
+++ core/security.py
@@ -1,9 +1,16 @@
 """
 Contains functions that implement the hash and encrypt processing.
 
+
 Functions:
+    def hash_password(password: str) -> str:
+    = hash password and return hash in str type
     --------------------------------------------------------------------------------------------------------------------
-        ...
+    def match_password_with_hash(password: str, hashed_password: str) -> bool:
+    = match input password with the hash
+    --------------------------------------------------------------------------------------------------------------------
+    def generate_secret_key() -> bytes:
+    = generate secret key (might be used for cookie_storage)
     --------------------------------------------------------------------------------------------------------------------
 """
 
@@ -13,37 +20,24 @@ import cryptography.fernet
 
 
 def hash_password(password: str) -> str:
-    """ Hash input data by bcrypt """
+    """ Hash password by bcrypt """
     encoded_password = password.encode()
     salt = bcrypt.gensalt()
     hashed_password_bytes = bcrypt.hashpw(encoded_password,  salt)
     hashed_password = hashed_password_bytes.decode()
-    print(hashed_password)
-    print(type(hashed_password))
     return hashed_password
 
 
-def compare_password_with_hash(password: str, hashed_password: str) -> bool:
+def match_password_with_hash(password: str, hashed_password: str) -> bool:
     """ Check the password on compliance with the hash """
     password = password.encode()
     hashed_password = hashed_password.encode()
-
-    if bcrypt.checkpw(password, hashed_password):
-        print("It Matches!")
-        return False
-    else:
-        print("It Does not Match :(")
-        return True
+    result_status = bcrypt.checkpw(password, hashed_password)
+    return result_status
 
 
 def generate_secret_key() -> bytes:
-    """ Generate secret key for session-cookie storage """
+    """ Generate secret key (might be used for session-cookie storage) """
     fernet_key = cryptography.fernet.Fernet.generate_key()
     secret_key = base64.urlsafe_b64decode(fernet_key)
     return secret_key
-
-
-# if __name__ == '__main__':
-#     password = 'super_secret_key'
-#     hashed_password = hash_password(password)
-#     compare_password_with_hash(password, hashed_password)
