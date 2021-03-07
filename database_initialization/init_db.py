c8eb4a6873f36df6a98267102caafac07b34538b
Max Zhenzhera <megashurik@urk.net>
2021-03-07 17:35:04 +0200

    added templates, divided on folders by topics

--- database_initialization/init_db.py
+++ database_initialization/init_db.py
@@ -43,13 +43,17 @@ DB_ROOT_PASSWORD: str = DATABASE_CONFIG['root_password']
 
 async def setup_db(connection: aiomysql.Connection) -> None:
     """ Create database, tables and user """
-    query_create_database = Database.create_database.format(
-        db_name=DB_NAME
-    )
+    query_to_create_database = Database.create_database
+    params = {
+        'db_name': DB_NAME
+    }
+
+    query = query_to_create_database.format(**params)
 
     async with connection.cursor() as cursor:
-        await cursor.execute(query_create_database)
-        logger.success('Database is created.')
+        await cursor.execute(query)
+
+    logger.success('Database is created.')
 
     await create_tables(connection)
     await create_user(connection)
@@ -57,62 +61,90 @@ async def setup_db(connection: aiomysql.Connection) -> None:
 
 async def teardown_db(connection: aiomysql.Connection) -> None:
     """ Drop the entire base """
-    query_drop_database = Database.drop_database.format(
-        db_name=DB_NAME
-    )
+    query_to_drop_database = Database.drop_database
+    params = {
+        'db_name': DB_NAME
+    }
+
+    query = query_to_drop_database.format(**params)
 
     async with connection.cursor() as cursor:
-        await cursor.execute(query_drop_database)
-        logger.success('Database is dropped.')
+        await cursor.execute(query)
+
+    logger.success('Database is dropped.')
 
     await drop_user(connection)
 
 
 async def create_tables(connection: aiomysql.Connection) -> None:
     """ Create all tables in the database """
-    query_use_database = "USE {db_name}".format(db_name=DB_NAME)
+    query_to_use_database = Database.use_database
+    params = {
+        'db_name': DB_NAME
+    }
+
+    query_to_use_database = query_to_use_database.format(**params)
 
     async with connection.cursor() as cursor:
-        await cursor.execute(query_use_database)
+        await cursor.execute(query_to_use_database)
+
         for table in tables:
-            await cursor.execute(table.create_table)
-        logger.success('Tables are created.')
+            query_to_create_table = table.create_table
+            await cursor.execute(query_to_create_table)
+
+    logger.success('Tables are created.')
 
 
 async def drop_tables(connection: aiomysql.Connection) -> None:
     """ Drop all tables in the database """
-    query_use_database = "USE {db_name}".format(db_name=DB_NAME)
+    query_to_use_database = Database.use_database
+    params = {
+        'db_name': DB_NAME
+    }
+
+    query_to_use_database = query_to_use_database.format(**params)
 
     async with connection.cursor() as cursor:
-        await cursor.execute(query_use_database)
+        await cursor.execute(query_to_use_database)
+
         for table in tables:
-            await cursor.execute(table.drop_table)
-        logger.success('Tables are dropped.')
+            query_to_drop_table = table.drop_table
+            await cursor.execute(query_to_drop_table)
+
+    logger.success('Tables are dropped.')
 
 
 async def create_user(connection: aiomysql.Connection) -> None:
     """ Create user that lead the app database """
-    query_create_user = Database.create_user.format(
-        db_name=DB_NAME,
-        host=DB_HOST,
-        user_name=DB_USER_NAME,
-        user_password=DB_USER_PASSWORD
-    )
+    query_to_create_user = Database.create_user
+    params = {
+        'db_name': DB_NAME,
+        'host': DB_HOST,
+        'user_name': DB_USER_NAME,
+        'user_password': DB_USER_PASSWORD
+    }
+
+    query = query_to_create_user.format(**params)
 
     async with connection.cursor() as cursor:
-        await cursor.execute(query_create_user)
-        logger.success('UserCreation is created.')
+        await cursor.execute(query)
+
+    logger.success('User is created.')
 
 
 async def drop_user(connection: aiomysql.Connection) -> None:
     """ Drop user that lead the app database """
-    query_drop_user = Database.drop_user.format(
-        user_name=DB_USER_NAME,
-    )
+    query_to_drop_user = Database.drop_user
+    params = {
+        'user_name': DB_USER_NAME
+    }
+
+    query = query_to_drop_user.format(**params)
 
     async with connection.cursor() as cursor:
-        await cursor.execute(query_drop_user)
-        logger.success('UserCreation is dropped.')
+        await cursor.execute(query, params)
+
+    logger.success('User is dropped.')
 
 
 async def main() -> None:
