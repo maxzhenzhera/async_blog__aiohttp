f9cbc1e4c64cfc49dc030589f6a69051f5f403f2
Max Zhenzhera <megashurik@urk.net>
2021-03-07 17:42:31 +0200

    added handling other errors; refactored

--- core/middlewares.py
+++ core/middlewares.py
@@ -1,11 +1,6 @@
 """
 Contains middlewares that wrap server work.
 
-
-.. exception:: ServerError(Exception)
-    catch any errors raised while the app working
-
-
 .. function:: create_error_middleware(overrides) -> Callable
     create error middleware
 .. function:: create_session_middleware() -> Callable
@@ -24,16 +19,15 @@ from typing import (
 import aiohttp.web
 import aiohttp_session
 import aiohttp_session.cookie_storage
-from loguru import logger
+import pymysql
 
 from . import security
 from .database import db
-from .views import custom_errors
+from .views import (
+    InvalidFormDataError,
+    custom_errors
+)
 from .views.auth import AuthenticationError
-
-
-class ServerError(Exception):
-    """ Catch any errors raised while the app working """
 
 
 def create_error_middleware(overrides: dict[int, Callable]) -> Callable:
@@ -64,32 +58,71 @@ def create_error_middleware(overrides: dict[int, Callable]) -> Callable:
         try:
             handler_result = await handler(request)
 
-        # it is not information in database - record is not found
+        # it is invalid form data - it is the critical error for form data (that will be used in some processing)
+        # (for difference between validation error and this check documentation)
+        except InvalidFormDataError:
+            if 400 in overrides:
+                return await overrides[400](request)
+
+            raise aiohttp.web.HTTPBadRequest
+        # - - -
+
+        # - - - - - - - - -
+
+        # database errors (integrity errors, nonexistent data that saved in the database error) handling
+
+        # # it is incorrect form data (try to relate nonexistent data) - invoke database integrity errors
+        except pymysql.err.IntegrityError as error:
+            if 400 in overrides:
+                return await overrides[400](request)
+
+            raise aiohttp.web.HTTPBadRequest
+        # - - -
+
+        # # it is not information in database (try to get nonexistent data) - record is not found
         except db.RecordNotFoundError:
             if 404 in overrides:
                 return await overrides[404](request)
             raise aiohttp.web.HTTPNotFound
         # - - -
 
-        # user is not verified - user has not access to action
+        # - - - - - - - - -
+
+        # user access errors handling
+
+        # # user is not verified - user has not access to action
         except AuthenticationError:
-            raise aiohttp.web.HTTPForbidden
+            if 403 in overrides:
+                return await overrides[403](request)
+            raise aiohttp.web.HTTPNotFound
         # - - -
 
-        # handling custom http errors
+        # - - - - - - - - -
+
+        # custom errors handling
+
+        # # handling custom http errors
         except aiohttp.web.HTTPException as error:
             override = overrides.get(error.status)
             if override:
                 return await override(request)
+
+            raise
         # - - -
 
-            # debug mod
-            raise
+        except Exception as error:
+            request.app['logger'].error('Error raised while server is working with message: {}'.format(str(error)))
+            request.app['logger'].exception(error)
 
-        # except ServerError:
-        #     return await overrides[500](request)
+            # debug - - |
+            # raise
+            # - - - - - |
+
+            return await overrides[500](request)
         else:
             return handler_result
+
+        # - - - - - - - - -
 
     return error_middleware
 
@@ -144,14 +177,14 @@ def create_log_middleware() -> Callable:
             handler_result = await handler(request)
         except aiohttp.web.HTTPException as error:
             message = message.format(request.remote, str(request.url), request.method, error.status)
-            logger.info(message)
+            request.app['logger'].info(message)
 
             raise error
         else:
             # pass static downloads
-            if 'static' not in request:
+            if 'static' not in str(request.url):
                 message = message.format(request.remote, str(request.url), request.method, 200)
-                logger.info(message)
+                request.app['logger'].info(message)
 
             return handler_result
 
@@ -171,12 +204,12 @@ def setup_middlewares(app: aiohttp.web.Application) -> None:
 
     error_middleware = create_error_middleware(overrides=custom_errors.errors)
     session_middleware = create_session_middleware()
-    # log_middleware = create_log_middleware()
+    log_middleware = create_log_middleware()
 
     middlewares = [
         error_middleware,
         session_middleware,
-        # log_middleware
+        log_middleware
     ]
 
     app.middlewares.extend(middlewares)
