1f24ae23f1e4a49aac4889bf8e62e808c9cba914
Max Zhenzhera <megashurik@urk.net>
2021-03-07 17:43:45 +0200

    added new global settings

--- core/settings.py
+++ core/settings.py
@@ -19,11 +19,23 @@ import pathlib
 
 
 BASE_DIR = pathlib.Path(__file__).parent.parent
+
+LOGS_DIR = BASE_DIR / 'logs'
+ERROR_LOG = LOGS_DIR / 'errors.log'
+
+CORE_DIR = pathlib.Path(__file__).parent
+
+STATIC_DIR = CORE_DIR / 'static'
+IMAGES_DIR = STATIC_DIR / 'images'
+USER_IMAGES_DIR = IMAGES_DIR / 'user_images'
+
 DEFAULT_CONFIG_PATH = BASE_DIR / 'config' / 'config.ini'
 
 # default values for entire project
 DEFAULT_POSTS_ON_PAGE = 10
-DEFAULT_NOTES_ON_PAGE = 20
+DEFAULT_NOTES_ON_PAGE = 25
+
+DEFAULT_PAGE_NUMBERS_SEPARATOR = '...'
 
 
 def get_config() -> configparser.ConfigParser:
