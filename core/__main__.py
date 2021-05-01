b15d6bfd5108c970b3832baa213ca71a2c19e13f
Max Zhenzhera <megashurik@urk.net>
2021-05-01 11:24:56 +0300

    added package to global path

--- core/__main__.py
+++ core/__main__.py
@@ -2,8 +2,16 @@
 Entry point.
 """
 
-from core.main import main
+# add package to global path -------------------------------------------------------------------------------------------
+import sys
+import pathlib
+
+
+sys.path.append(pathlib.Path(__file__).parent.parent.__str__())
+# ----------------------------------------------------------------------------------------------------------------------
 
 
 if __name__ == '__main__':
+    from core.main import main
+
     main()
