6142b6c6523cf98ab1150a08e2d26eb6713f2d52
Max Zhenzhera <megashurik@urk.net>
2021-05-01 11:24:21 +0300

    config migrated to .env file; deleted db init [generating] files

--- core/settings.py
+++ core/settings.py
@@ -1,52 +1,62 @@
 """
 Contains settings data (paths, config).
 
-.. function:: get_config() -> configparser.ConfigParser
-    Parse .ini file with settings in ConfigParser object that imitate `dict` work
+.. data:: CORE_DIR
+.. data:: BASE_DIR
+.. data:: STATIC_DIR
+.. data:: IMAGES_DIR
+.. data:: USER_IMAGES_DIR
 
-.. const:: BASE_DIR: pathlib.Path
-    Contains path to project directory
-.. const:: DEFAULT_CONFIG_PATH: pathlib.Path
-    Contains path to config
-.. const:: DEFAULT_POSTS_ON_PAGE
-    Default value of the posts quantity on page
-.. const:: DEFAULT_NOTES_ON_PAGE
-    Default value of the notes quantity on page
+.. data:: SERVER_HOST
+.. data:: SERVER_PORT
+
+.. data:: DB_NAME
+.. data:: DB_USER
+.. data:: DB_PASSWORD
+.. data:: DB_HOST
+.. data:: DB_PORT
+
+.. data:: WEBSITE_ADMIN_LOGIN
+.. data:: WEBSITE_ADMIN_PASSWORD
+
+.. data:: DEFAULT_POSTS_ON_PAGE
+.. data:: DEFAULT_NOTES_ON_PAGE
+.. data:: DEFAULT_PAGE_NUMBERS_SEPARATOR
 """
 
-import configparser
+import os
 import pathlib
 
+from dotenv import load_dotenv
 
-BASE_DIR = pathlib.Path(__file__).parent.parent
 
-LOGS_DIR = BASE_DIR / 'logs'
-ERROR_LOG = LOGS_DIR / 'errors.log'
+load_dotenv()
+
 
 CORE_DIR = pathlib.Path(__file__).parent
-
+BASE_DIR = CORE_DIR.parent
 STATIC_DIR = CORE_DIR / 'static'
 IMAGES_DIR = STATIC_DIR / 'images'
 USER_IMAGES_DIR = IMAGES_DIR / 'user_images'
 
-DEFAULT_CONFIG_PATH = BASE_DIR / 'config' / 'config.ini'
+
+SERVER_HOST = os.getenv('SERVER_HOST')
+SERVER_PORT = int(os.getenv('SERVER_PORT'))
+
+DB_NAME = os.getenv('DB_NAME')
+DB_USER = os.getenv('DB_USER')
+DB_PASSWORD = os.getenv('DB_PASSWORD')
+DB_HOST = os.getenv('DB_HOST')
+DB_PORT = int(os.getenv('DB_PORT'))
+
+
+WEBSITE_ADMIN_LOGIN = os.getenv('WEBSITE_ADMIN_LOGIN')
+WEBSITE_ADMIN_PASSWORD = os.getenv('WEBSITE_ADMIN_PASSWORD')
+
 
 # default values for entire project
 DEFAULT_POSTS_ON_PAGE = 10
 DEFAULT_NOTES_ON_PAGE = 25
 
 DEFAULT_PAGE_NUMBERS_SEPARATOR = '...'
-
-
-def get_config() -> configparser.ConfigParser:
-    """
-    Get config from config file.
-
-    :return: config object with interface like dict
-    :rtype: configparser.ConfigParser
-    """
-
-    config = configparser.ConfigParser()
-    config.read(DEFAULT_CONFIG_PATH)
-
-    return config
+# - - -
